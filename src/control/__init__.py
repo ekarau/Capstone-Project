8ab31439cd2ce407aebedbc960a9f7f8450be693
ekarau <karaurganege@gmail.com>
2026-05-18 17:10:33 +0300

    remove unused YOLOv8Detector, ElevatorController, Detection dataclass

--- src/control/__init__.py
+++
@@ -1,7 +0,0 @@
-from src.control.algorithm import (
-    ControlDecision,
-    ControlResult,
-    ElevatorController,
-)
-
-__all__ = ["ControlDecision", "ControlResult", "ElevatorController"]
--- src/detection/__init__.py
+++
@@ -1,3 +0,0 @@
-from src.detection.detector import Detection, YOLOv8Detector
-
-__all__ = ["Detection", "YOLOv8Detector"]
